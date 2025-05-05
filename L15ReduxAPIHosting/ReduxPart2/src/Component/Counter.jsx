828aa0d6158f1f8a9c7ae746d763813c4f5c1fbe
“Aamir <aamirhussainazad786@gmail.com>
2025-05-05 13:05:26 +0530

    commit add

--- L15ReduxAPIHosting/ReduxPart2/src/Component/Counter.jsx
+++ L15ReduxAPIHosting/ReduxPart2/src/Component/Counter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 // import { setCountInterval } from '../Redux/actions'; 
-import { setCountInterval } from '../Redux/Slices/counterSlice'; 
+import { setCountInterval } from '../Redux/Slices/counterSlice';
 
 
 
@@ -14,21 +14,31 @@ const Counter = () => {
 
     const handleIncrement = () => {
         dispatch(setCountInterval({ count: count + 1 }));
+
+    };
+    const handleDecrement = () => {
+        dispatch(setCountInterval({ count: count - 1 }));
+
+
     };
     return (
         <div>
             <h1>count</h1>
             <h2>{count}</h2>
-            <button onClick={() => dispatch(setCountInterval({count: 0}))}>Reset</button>
-            <button onClick={() => dispatch(setCountInterval({count: count + 1}))}>Increment</button>
+            <button onClick={() => dispatch(setCountInterval({ count: 0 }))}>Reset</button>
+            <button onClick={() => dispatch(setCountInterval({ count: count + 1 }))}>Increment</button>
             <button onClick={() => dispatch(setCountInterval({ count: count - 1 }))}>Decrement</button>
-            
+
 
             <button onClick={() => dispatch(setCountInterval({ count: count + 5 }))}>Increment by 5</button>
             <button onClick={() => dispatch(setCountInterval({ count: count - 5 }))}>Decrement by 5</button>
 
+
+            <button onClick={handleIncrement}>+</button>
+            {/* <button onClick={() => dispatch(setCountInterval({ count: count - 1 }))}>-</button> */}
             
-            <button onClick={handleIncrement }>+</button>
+            <button onClick={handleDecrement}>-</button>
+        
 
         </div>
     );
--- L15ReduxAPIHosting/ReduxPart2/src/Redux/Slices/counterSlice.js
+++ L15ReduxAPIHosting/ReduxPart2/src/Redux/Slices/counterSlice.js
@@ -1,15 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-    count: 0
-}
+// import { setCountInterval } from '../actions/counterActions'; // Removed unused import to fix the error
+
+
+// import {setCountIncrement, setCountDecrement } from '../Redux/Slices/counterSlice';
+
+// import { setCountIncrement, setCountDecrement } from '../actions/counterActions';
+
+const initialState = {count: 0}
 
 const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
+        //1st way to update the state
         setCountInterval: (state, action) => {
             state.count = action.payload.count;
+            // state.count = state.count +action.payload + 1;
+            // state.count = action.payload.count + 1; // Increment by 100vh
+        },
+
+        //2nd way to update the state
+        //originally we can use this way to update the state
+
+        setCountIncrement: (state) => {
+            state.count++;
+        },
+        setCountDecrement: (state) => {
+            state.count--;
         }
     },
 })
