1f1e87efc735577858edf8ae978fa000e98715f6
“Aamir <aamirhussainazad786@gmail.com>
2025-05-06 22:42:26 +0530

    commit add

--- L15ReduxAPIHosting/API_Integration/src/App.jsx
+++ L15ReduxAPIHosting/API_Integration/src/App.jsx
@@ -1,23 +1,53 @@
+// import React from 'react';
+// import './App.css';
+// import Counter from './Component/Counter';
+// import User from './Component/User';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import store from './Redux/Store/store.js'; // make sure this path is correct
+// import Home from './Component/API';
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <div>
+//           <Routes>
+//             <Route path="/" element={<Counter />} />
+//             <Route path="/user" element={<User />} />
+//           </Routes>
+//         </div>
+//       </BrowserRouter>
+//     </Provider>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import './App.css';
 import Counter from './Component/Counter';
 import User from './Component/User';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './Redux/Store/store.js'; // make sure this path is correct
+import Home from './Component/API';
 
 function App() {
   return (
-    <Provider store={store}>
+
       <BrowserRouter>
         <div>
-          <Routes>
-            <Route path="/" element={<Counter />} />
-            <Route path="/user" element={<User />} />
-          </Routes>
+          Api
         </div>
       </BrowserRouter>
-    </Provider>
   );
 }
 
--- L15ReduxAPIHosting/API_Integration/src/Component/API.jsx
+++ L15ReduxAPIHosting/API_Integration/src/Component/API.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+
+const fetchData = async () => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
 
 const API = () => {
     return (
         <div>
-
+            <h1>API Integration</h1>
+            <p>This is a simple API integration example.</p>
+            <button onClick={fetchData}>Fetch Data</button>
+            <p>Check the console for API response.</p>
         </div>
     )
 }
