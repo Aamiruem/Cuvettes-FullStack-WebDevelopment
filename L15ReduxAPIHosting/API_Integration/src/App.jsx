0cc3a00632b49f89a12f6a97f5f7be41bf551651
“Aamir <aamirhussainazad786@gmail.com>
2025-05-06 19:03:09 +0530

    commit add

--- L15ReduxAPIHosting/API_Integration/src/App.jsx
+++ L15ReduxAPIHosting/API_Integration/src/App.jsx
@@ -1,28 +1,10 @@
-// import React from 'react';
-// import { Provider } from 'react-redux';
-// import store from './Redux/Store/store';
-// import Counter from './Component/Counter';
-
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//       <Counter />
-//     </Provider>
-//   );
-// };
-
-// export default App;
-
-
-
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './Redux/Store/store';
+import './App.css';
 import Counter from './Component/Counter';
 import User from './Component/User';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from './Redux/Store/store.js'; // make sure this path is correct
 
 function App() {
   return (
---
+++ L15ReduxAPIHosting/API_Integration/src/main.jsx
@@ -0,0 +1,21 @@
+
+import './index.css'
+import App from './App.jsx'
+import React from 'react'
+
+import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux";
+import { store } from './Redux/Store/store.js'
+// import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <BrowserRouter> */}
+        <App />
+      {/* </BrowserRouter> */}
+    </Provider>
+  </React.StrictMode>,
+)
