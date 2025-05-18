91aea941c15fb1b4371f17a651fb0dab4ab1aa20
Aamir@786 <aamirhussainazad786@gmail.com>
2025-05-18 23:08:21 +0530

    commit add

--- L15ReduxAPIHosting/CRUDFormValidation/src/Redux/State/userSlice.js
+++ L15ReduxAPIHosting/CRUDFormValidation/src/Redux/State/userSlice.js
@@ -8,9 +8,9 @@
 //     reducer: {
 //         counter: counterReducer,
 //         user: userReducer,
-        
-        
-        
+
+
+
 //     }
 // });
 
@@ -21,39 +21,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  user: {
-    id: '',
-    name: '',
-    email: '',
-    phone: '',
-    password: ''
-  },
-  users: []
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        phone: '',
+        password: ''
+    },
+    users: []
 };
 
 export const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    setUser: (state, action) => {
-      state.user = action.payload;
-    },
-    setUsers: (state, action) => {
-      state.users.push(action.payload);
-    },
-    editUser: (state, action) => {
-      const index = state.users.findIndex(user => user.id === action.payload.id);
-      if (index !== -1) {
-        state.users[index] = action.payload;
-      }
-    },
-    deleteUser: (state, action) => {
-      state.users = state.users.filter(user => user.id !== action.payload);
-    },
-    resetUser: (state) => {
-      state.user = initialState.user;
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        setUsers: (state, action) => {
+            state.users.push(action.payload);
+        },
+        editUser: (state, action) => {
+            const index = state.users.findIndex(user => user.id === action.payload.id);
+            if (index !== -1) {
+                state.users[index] = action.payload;
+            }
+        },
+        deleteUser: (state, action) => {
+            state.users = state.users.filter(user => user.id !== action.payload);
+        },
+        resetUser: (state) => {
+            state.user = initialState.user;
+        }
     }
-  }
 });
 
 export const { setUser, setUsers, editUser, deleteUser, resetUser } = userSlice.actions;
