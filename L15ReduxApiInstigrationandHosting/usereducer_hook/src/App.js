2c86a0df2e552d03d27baa57e23b18378d027a30
Aamir@786 <aamirhussainazad786@gmail.com>
2025-05-20 23:40:21 +0530

    commit add

--- L15ReduxApiInstigrationandHosting/usereducer_hook/src/App.js
+++ L15ReduxApiInstigrationandHosting/usereducer_hook/src/App.js
@@ -25,20 +25,43 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  // const [name, setName] = useState('');
-  const handleIncrement = () => {
-    setCount(count + 1);
-    // setCount(count * 2);
-  }
+  // const [count, setCount] = useState(0);
+  // // const [name, setName] = useState('');
+  // const handleIncrement = () => {
+  //   setCount(count + 1);
+  //   // setCount(count * 2);
+  // }
 
+  // const handleDecrement = () => {
+  //   setCount(count - 1);
+  //   // setCount(count % 2);
+  // }
+
+
+
+  const initialState = {
+    count: 0,
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+  const [state, setState] = useState(initialState);
+  const handleIncrement = () => {
+    setState({
+      ...state,
+      count: state.count + 1,
+    });
+  }
   const handleDecrement = () => {
-    setCount(count - 1);
-    setCount(count % 2);
+    setState({
+      ...state,
+      count: state.count - 1,
+    });
   }
   return (
     <div className="App">
-      <h1>Count: {count}</h1>
+      <h1>Count: {state.count}</h1>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
       
