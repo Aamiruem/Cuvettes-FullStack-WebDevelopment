658ce56a114efa83db32c68c4ade68d389fbaa9f
Aamir@786 <aamirhussainazad786@gmail.com>
2025-05-25 15:38:31 +0530

    commit add

---
+++ L15ReduxApiInstigrationandHosting/react_form/src/Component/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <h1>User Data</h1>
+            <form>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
