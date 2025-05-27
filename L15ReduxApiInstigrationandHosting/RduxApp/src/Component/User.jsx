99819661e015c9e6aacb4de932c90d57c304c29b
Aamir@786 <aamirhussainazad786@gmail.com>
2025-05-27 23:18:13 +0530

    commit add

--- L15ReduxApiInstigrationandHosting/RduxApp/src/Component/User.jsx
+++ L15ReduxApiInstigrationandHosting/RduxApp/src/Component/User.jsx
@@ -1,12 +1,142 @@
+// import React, { useState } from 'react';
+// import {useSelector} from "react-redux";
+// import { useDispatch } from "react-redux";
+
+// const User = () => {
+//     const data = useSelector(state => state.user.user);
+//     console.log("User data from Redux store:", data);
+//     const dispatch = useDispatch();
+
+//     const [user, setUser] = useState({
+//         name: '',
+//         email: '',
+//         phone: '',
+//         username: '',
+//         password: '',
+//         confirmPassword: '',
+//         age: ''
+//     });
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setUser(prev => ({
+//             ...prev,
+//             [name]: value
+//         }));
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         console.log("Form submitted:", user);
+//         dispatch({ type: 'SET_USER', payload: user });
+//         setMainUser({
+//             name: '',
+//             email: '',
+//             phone: '',
+//             username: '',
+//             password: '',
+//             confirmPassword: '',
+//             age: ''
+//         });
+//     };
+
+//     return (
+//         <div className='user'>
+//             <h1>User</h1>
+
+//             <form onSubmit={handleSubmit}>
+//                 <input
+//                     type="text"
+//                     placeholder="Enter your name"
+//                     name="name"
+//                     value={user.name}
+//                     onChange={handleChange}
+//                 />
+//                 <br /><br />
+
+//                 <input
+//                     type="text"
+//                     placeholder="Enter your username"
+//                     name="username"
+//                     value={user.username}
+//                     onChange={handleChange}
+//                 />
+//                 <br /><br />
+
+//                 <input
+//                     type="email"
+//                     placeholder="Enter your email"
+//                     name="email"
+//                     value={user.email}
+//                     onChange={handleChange}
+//                 />
+//                 <br /><br />
+
+//                 <input
+//                     type="password"
+//                     placeholder="Enter your password"
+//                     name="password"
+//                     value={user.password}
+//                     onChange={handleChange}
+//                 />
+//                 <br /><br />
+
+//                 <input
+//                     type="password"
+//                     placeholder="Confirm your password"
+//                     name="confirmPassword"
+//                     value={user.confirmPassword}
+//                     onChange={handleChange}
+//                 />
+//                 <br /><br />
+
+//                 <input
+//                     type="tel"
+//                     placeholder="Enter your phone number"
+//                     name="phone"
+//                     value={user.phone}
+//                     onChange={handleChange}
+//                 />
+//                 <br /><br />
+
+//                 <input
+//                     type="number"
+//                     placeholder="Enter your age"
+//                     name="age"
+//                     value={user.age}
+//                     onChange={handleChange}
+//                 />
+//                 <br /><br />
+
+//                 <button onClick={handleSubmit} className='btn' type="submit">Submit</button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default User;
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState } from 'react';
-import {useSelector} from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
+import { setMainUser, setUsers } from '../Redux/Slices/userSlice';
+
+
 
 const User = () => {
     const data = useSelector(state => state.user.user);
     console.log("User data from Redux store:", data);
     const dispatch = useDispatch();
-    
+
     const [user, setUser] = useState({
         name: '',
         email: '',
@@ -28,6 +158,25 @@ const User = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("Form submitted:", user);
+
+        dispatch({ type: 'SET_USER', payload: user });
+
+        // Reset form
+        setUser({
+            name: '',
+            email: '',
+            phone: '',
+            username: '',
+            password: '',
+            confirmPassword: '',
+            age: ''
+        });
+
+
+        dispatch(setMainUser(user));
+        dispatch(setUsers(user));
+        console.log("User data set in Redux store:", user);
+
     };
 
     return (
@@ -35,22 +184,18 @@ const User = () => {
             <h1>User</h1>
 
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
+                <input type="text"
                     placeholder="Enter your name"
                     name="name"
                     value={user.name}
-                    onChange={handleChange}
-                />
+                    onChange={handleChange} />
                 <br /><br />
 
-                <input
-                    type="text"
+                <input type="text"
                     placeholder="Enter your username"
                     name="username"
                     value={user.username}
-                    onChange={handleChange}
-                />
+                    onChange={handleChange} />
                 <br /><br />
 
                 <input
@@ -59,7 +204,9 @@ const User = () => {
                     name="email"
                     value={user.email}
                     onChange={handleChange}
+                    autoComplete="email"
                 />
+
                 <br /><br />
 
                 <input
@@ -68,37 +215,37 @@ const User = () => {
                     name="password"
                     value={user.password}
                     onChange={handleChange}
+                    autoComplete="new-password"
                 />
-                <br /><br />
 
+                <br /><br />
                 <input
                     type="password"
                     placeholder="Confirm your password"
                     name="confirmPassword"
                     value={user.confirmPassword}
                     onChange={handleChange}
+                    autoComplete="new-password"
                 />
+
                 <br /><br />
 
-                <input
-                    type="tel"
+                <input type="tel"
                     placeholder="Enter your phone number"
                     name="phone"
                     value={user.phone}
-                    onChange={handleChange}
-                />
+                    onChange={handleChange} />
                 <br /><br />
 
-                <input
-                    type="number"
-                    placeholder="Enter your age"
-                    name="age"
+                <input type="number"
+                    placeholder="Enter your age" name="age"
                     value={user.age}
-                    onChange={handleChange}
-                />
+                    onChange={handleChange} />
                 <br /><br />
 
-                <button onClick={handleSubmit} className='btn' type="submit">Submit</button>
+                <button onClick={handleSubmit}
+                    type="submit"
+                    className='btn'>Submit</button>
             </form>
         </div>
     );
--- L15ReduxApiInstigrationandHosting/RduxApp/src/Redux/Slices/userSlice.js
+++ L15ReduxApiInstigrationandHosting/RduxApp/src/Redux/Slices/userSlice.js
@@ -1,7 +1,45 @@
-import React from 'react'
-import { createSlice } from '@reduxjs/toolkit';
-import { useSelector, useDispatch } from "react-redux";
+// import React from 'react'
+// import { createSlice } from '@reduxjs/toolkit';
+// import { useSelector, useDispatch } from "react-redux";
 
+// const initialState = {
+//     user: {
+//         name: '',
+//         email: '',
+//         phone: '',
+//         username: '',
+//         password: '',
+//         confirmPassword: '',
+//         age: ''
+//     }
+// };
+// const userSlice = createSlice({
+//     name: 'user',
+//     initialState,
+//     reducers: {
+//         setMainUser(state, action) {
+//             state.user = action.payload;
+//         }
+//     }
+// });
+
+// export default userSlice.reducer;
+// export const { setMainUser } = userSlice.actions;
+
+
+
+
+
+
+
+
+
+
+
+
+import { createSlice } from '@reduxjs/toolkit';
+
+// Initial state for the user
 const initialState = {
     user: {
         name: '',
@@ -11,17 +49,28 @@ const initialState = {
         password: '',
         confirmPassword: '',
         age: ''
-    }
+    },
+    isActive: false, users:[]
 };
+
+// Creating the slice
 const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        setUser(state, action) {
+        // Reducer to update the user in the state
+        setMainUser(state, action) {
             state.user = action.payload;
-        }   
+        },
+        // Reducer to set the active status of the user
+        setUsers(state, action) {
+            state.users.push(action.payload);
+        }
     }
 });
 
+// Export the reducer to be added to the store
 export default userSlice.reducer;
-export const { setUser } = userSlice.actions;
+
+// Export the action to use in components
+export const { setMainUser, setUsers } = userSlice.actions;
