0bfd3af2c60c7492e754459493b6220d3afd8ace
Aamir@786 <aamirhussainazad786@gmail.com>
2025-05-28 20:19:03 +0530

    commit add

--- L15ReduxApiInstigrationandHosting/RduxApp/src/Component/User.jsx
+++ L15ReduxApiInstigrationandHosting/RduxApp/src/Component/User.jsx
@@ -126,16 +126,156 @@
 
 
 
+// import React, { useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { setMainUser, setUsers, setIsActive } from '../Redux/Slices/userSlice';
+
+
+
+// const User = () => {
+//     const users = useSelector(state => state.user.users);
+//     console.log("User data from Redux store:", users);
+//     const dispatch = useDispatch();
+//     const isActive = useSelector(state => state.user.isActive);
+//     console.log("Is user active:", isActive);
+
+//     const [user, setUser] = useState({
+//         name: '',
+//         email: '',
+//         phone: '',
+//         username: '',
+//         password: '',
+//         confirmPassword: '',
+//         age: ''
+//     });
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setUser(prev => ({
+//             ...prev,
+//             [name]: value
+//         }));
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         console.log("Form submitted:", user);
+
+//         dispatch({ type: 'SET_USER', payload: user });
+
+//         // Reset form
+//         setUser({
+//             name: '',
+//             email: '',
+//             phone: '',
+//             username: '',
+//             password: '',
+//             confirmPassword: '',
+//             age: ''
+//         });
+
+
+//         dispatch(setMainUser(user));
+//         dispatch(setUsers(user));
+//         console.log("User data set in Redux store:", user);
+//         dispatch(setIsActive(true));
+//         console.log("User is active:", true);
+
+//     };
+
+//     return (
+//         <div className='user'>
+//             <h1>User</h1>
+//             <h1>Data Submitted</h1>
+
+//             {
+//                 isActive ? <h2>User is active</h2> : <p>User is not active</p>
+//             }
+
+//             <form onSubmit={handleSubmit}>
+//                 <input type="text"
+//                     placeholder="Enter your name"
+//                     name="name"
+//                     value={user.name}
+//                     onChange={handleChange} />
+//                 <br /><br />
+
+//                 <input type="text"
+//                     placeholder="Enter your username"
+//                     name="username"
+//                     value={user.username}
+//                     onChange={handleChange} />
+//                 <br /><br />
+
+//                 <input
+//                     type="email"
+//                     placeholder="Enter your email"
+//                     name="email"
+//                     value={user.email}
+//                     onChange={handleChange}
+//                     autoComplete="email"
+//                 />
+
+//                 <br /><br />
+
+//                 <input
+//                     type="password"
+//                     placeholder="Enter your password"
+//                     name="password"
+//                     value={user.password}
+//                     onChange={handleChange}
+//                     autoComplete="new-password"
+//                 />
+
+//                 <br /><br />
+//                 <input
+//                     type="password"
+//                     placeholder="Confirm your password"
+//                     name="confirmPassword"
+//                     value={user.confirmPassword}
+//                     onChange={handleChange}
+//                     autoComplete="new-password"
+//                 />
+
+//                 <br /><br />
+
+//                 <input type="tel"
+//                     placeholder="Enter your phone number"
+//                     name="phone"
+//                     value={user.phone}
+//                     onChange={handleChange} />
+//                 <br /><br />
+
+//                 <input type="number"
+//                     placeholder="Enter your age" name="age"
+//                     value={user.age}
+//                     onChange={handleChange} />
+//                 <br /><br />
+
+//                 <button onClick={handleSubmit}
+//                     type="submit"
+//                     className='btn'>Submit</button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default User;
+
+
+
+
+
+
+
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { setMainUser, setUsers } from '../Redux/Slices/userSlice';
-
-
+import { setMainUser, setUsers, setIsActive } from '../Redux/Slices/userSlice';
 
 const User = () => {
-    const data = useSelector(state => state.user.user);
-    console.log("User data from Redux store:", data);
     const dispatch = useDispatch();
+    const users = useSelector(state => state.user.users);
+    const isActive = useSelector(state => state.user.isActive);
 
     const [user, setUser] = useState({
         name: '',
@@ -159,7 +299,12 @@ const User = () => {
         e.preventDefault();
         console.log("Form submitted:", user);
 
-        dispatch({ type: 'SET_USER', payload: user });
+        dispatch(setMainUser(user));
+        dispatch(setUsers(user));
+        dispatch(setIsActive(true));
+
+        console.log("User data set in Redux store:", user);
+        console.log("User is active:", true);
 
         // Reset form
         setUser({
@@ -171,32 +316,31 @@ const User = () => {
             confirmPassword: '',
             age: ''
         });
-
-
-        dispatch(setMainUser(user));
-        dispatch(setUsers(user));
-        console.log("User data set in Redux store:", user);
-
     };
 
     return (
         <div className='user'>
             <h1>User</h1>
+            <h2>Data Submitted</h2>
+
+            {isActive ? <h2>User is active</h2> : <p>User is not active</p>}
 
             <form onSubmit={handleSubmit}>
-                <input type="text"
+                <input
+                    type="text"
                     placeholder="Enter your name"
                     name="name"
                     value={user.name}
-                    onChange={handleChange} />
-                <br /><br />
+                    onChange={handleChange}
+                /><br /><br />
 
-                <input type="text"
+                <input
+                    type="text"
                     placeholder="Enter your username"
                     name="username"
                     value={user.username}
-                    onChange={handleChange} />
-                <br /><br />
+                    onChange={handleChange}
+                /><br /><br />
 
                 <input
                     type="email"
@@ -205,9 +349,7 @@ const User = () => {
                     value={user.email}
                     onChange={handleChange}
                     autoComplete="email"
-                />
-
-                <br /><br />
+                /><br /><br />
 
                 <input
                     type="password"
@@ -216,9 +358,8 @@ const User = () => {
                     value={user.password}
                     onChange={handleChange}
                     autoComplete="new-password"
-                />
+                /><br /><br />
 
-                <br /><br />
                 <input
                     type="password"
                     placeholder="Confirm your password"
@@ -226,26 +367,25 @@ const User = () => {
                     value={user.confirmPassword}
                     onChange={handleChange}
                     autoComplete="new-password"
-                />
+                /><br /><br />
 
-                <br /><br />
-
-                <input type="tel"
+                <input
+                    type="tel"
                     placeholder="Enter your phone number"
                     name="phone"
                     value={user.phone}
-                    onChange={handleChange} />
-                <br /><br />
+                    onChange={handleChange}
+                /><br /><br />
 
-                <input type="number"
-                    placeholder="Enter your age" name="age"
+                <input
+                    type="number"
+                    placeholder="Enter your age"
+                    name="age"
                     value={user.age}
-                    onChange={handleChange} />
-                <br /><br />
+                    onChange={handleChange}
+                /><br /><br />
 
-                <button onClick={handleSubmit}
-                    type="submit"
-                    className='btn'>Submit</button>
+                <button type="submit" className='btn'>Submit</button>
             </form>
         </div>
     );
--- L15ReduxApiInstigrationandHosting/RduxApp/src/Redux/Slices/userSlice.js
+++ L15ReduxApiInstigrationandHosting/RduxApp/src/Redux/Slices/userSlice.js
@@ -37,6 +37,79 @@
 
 
 
+// import { createSlice } from '@reduxjs/toolkit';
+
+// // Initial state for the user
+// const initialState = {
+//     user: {
+//         name: '',
+//         email: '',
+//         phone: '',
+//         username: '',
+//         password: '',
+//         confirmPassword: '',
+//         age: ''
+//     },
+//     isActive: false, users:[]
+// };
+
+// // Creating the slice
+// const userSlice = createSlice({
+//     name: 'user',
+//     initialState,
+//     reducers: {
+//         // Reducer to update the user in the state
+//         setMainUser(state, action) {
+//             state.user = action.payload;
+//         },
+//         // Reducer to set the active status of the user
+//         setUsers(state, action) {
+//             state.users.push(action.payload);
+//         },
+//         // setIsActive(state, action) {
+//         //     state.isActive = action.payload;
+//         // },
+
+//         // setActive: (state) => {
+//         //     state.isActive = !state.isActive;
+//         // }
+
+
+//         reducers: {
+//             setMainUser(state, action) {
+//                 state.user = action.payload;
+//             },
+//             setUsers(state, action) {
+//                 state.users.push(action.payload);
+//             },
+//             setIsActive(state, action) {
+//                 state.isActive = action.payload;
+//             }
+//         }
+        
+//     }
+// });
+
+// // Export the reducer to be added to the store
+// export default userSlice.reducer;
+
+// // Export the action to use in components
+// export const { setMainUser, setUsers, setIsActive } = userSlice.actions;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { createSlice } from '@reduxjs/toolkit';
 
 // Initial state for the user
@@ -50,7 +123,8 @@ const initialState = {
         confirmPassword: '',
         age: ''
     },
-    isActive: false, users:[]
+    isActive: false,
+    users: []
 };
 
 // Creating the slice
@@ -58,13 +132,17 @@ const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        // Reducer to update the user in the state
+        // Set the main user
         setMainUser(state, action) {
             state.user = action.payload;
         },
-        // Reducer to set the active status of the user
+        // Add a new user to the users array
         setUsers(state, action) {
             state.users.push(action.payload);
+        },
+        // Set user active status
+        setIsActive(state, action) {
+            state.isActive = action.payload;
         }
     }
 });
@@ -72,5 +150,5 @@ const userSlice = createSlice({
 // Export the reducer to be added to the store
 export default userSlice.reducer;
 
-// Export the action to use in components
-export const { setMainUser, setUsers } = userSlice.actions;
+// Export the actions to be used in components
+export const { setMainUser, setUsers, setIsActive } = userSlice.actions;
