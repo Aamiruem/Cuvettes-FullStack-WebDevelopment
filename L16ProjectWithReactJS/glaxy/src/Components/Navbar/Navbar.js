22687b411f86fbf3784ecd3efa205d2c80552919
Aamir@786 <aamirhussainazad786@gmail.com>
2025-06-22 23:51:23 +0530

    commit add

--- L16ProjectWithReactJS/glaxy/src/Components/Navbar/Navbar.js
+++ L16ProjectWithReactJS/glaxy/src/Components/Navbar/Navbar.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FaTimes, FaBars } from 'react-icons/fa';
+
+// import { FaHome } from 'react-icons/fa'; // for example
+
 const Navbar = () => {
+
+const [click, setClick] = React.useState(false);
+
+
+const handleClick = () => {
+    setClick(!click);
+}
+
     return (
         <nav>
             <div className="logo">
-                <h1>Galaxy</h1>
+                {/* <h1>Galaxy</h1> */}
             </div>
 
             <div className="menu">
@@ -14,6 +26,12 @@ const Navbar = () => {
                 <Link to="/training">Training</Link>
                 <Link to="/contact">Contact</Link>
             </div>
+            <div className="hamburger" onClick={handleClick}>
+                {/* <FaHome /> */}
+                {/* <FaTimes /> */}
+                <FaBars style={{ color: "white" }} size={25} />
+            </div>
+
         </nav>
     )
 }
--- L16ProjectWithReactJS/glaxy/src/Pages/Home/Home.js
+++ L16ProjectWithReactJS/glaxy/src/Pages/Home/Home.js
@@ -4,9 +4,13 @@ import Navbar from '../../Components/Navbar/Navbar';
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Galaxy Travels</h1>
             <Navbar />
+            <h2>Explore the Universe with Us</h2>
+
         </div>
+
+        
     )
 }
 
