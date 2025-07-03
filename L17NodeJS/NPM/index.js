36148da2c7c59af62af422182f17449eb29c267a
Aamir@786 <aamirhussainazad786@gmail.com>
2025-07-03 13:10:15 +0530

    commit add

---
+++ L17NodeJS/NPM/index.js
@@ -0,0 +1,10 @@
+// const url = require('url');
+
+// const address = "https://www.google.com";
+// const parsedUrl = url.parse(address);
+// console.log(parsedUrl);
+
+
+
+console.log("Hi from NPM");
+console.log("Hello from NPM");
