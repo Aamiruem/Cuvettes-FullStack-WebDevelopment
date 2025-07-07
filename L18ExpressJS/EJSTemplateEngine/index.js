0c777149b2acdff5bfabb7f09696f720218a7e58
Aamir@786 <aamirhussainazad786@gmail.com>
2025-07-07 18:41:53 +0530

    commit add

---
+++ L18ExpressJS/EJSTemplateEngine/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname) + "/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Your App Running and Listening on port ${port}`);
+});
