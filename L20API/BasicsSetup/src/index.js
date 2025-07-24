c92935bda51777aac05b67d4e3664181af7e041d
Aamir@786 <aamirhussainazad786@gmail.com>
2025-07-24 21:40:21 +0530

    commit add

--- L20API/BasicsSetup/src/index.js
+++ L20API/BasicsSetup/src/index.js
@@ -24,9 +24,9 @@ app.use(express.urlencoded({ extended: true }));
 
 
 //Post Request 
-app.post('/api/mentor', async (req, res) => {
+app.post('/api/mentors', async (req, res) => {
     try {
-        const mentor = await Mentor.create(req.body);
+        const mentor = await mentor.create(req.body);
         const createdMentor = await mentor.save();
         res.status(201).send(createdMentor);
     } catch (error) {
@@ -36,14 +36,41 @@ app.post('/api/mentor', async (req, res) => {
 
 
 // Get Request
-app.get('/api/mentor', async (req, res) => {
+app.get('/api/mentors/:id', async (req, res) => {
     try {
-        const mentorData = await Mentor.find({});
+        const mentorData = await mentor.find({});
         res.send(mentorData);
     } catch (error) {
         res.status(500).send(error);
     }
 });
+
+//patch request 
+app.patch('/api/mentors/:id', async (req, res) => {
+    try {
+        const mentor = await mentor.findByIdAndUpdate(req.params.id,
+            req.body,
+            { new: true });
+        res.send(mentor);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+
+// Delete Request
+app.delete('/api/mentors/:id', async (req, res) => {
+    try {
+        const mentor = await mentor.findByIdAndDelete(req.params.id);
+        if (!mentor) {
+            return res.status(404).send();
+        }
+        res.send(mentor);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
