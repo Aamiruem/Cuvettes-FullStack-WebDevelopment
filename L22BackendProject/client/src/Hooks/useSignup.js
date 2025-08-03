1a9f11baacd6ef6b27eee762f99cc503bc19e757
Aamir@786 <aamirhussainazad786@gmail.com>
2025-08-03 17:33:44 +0530

    commit add

--- L22BackendProject/client/src/Hooks/useSignup.js
+++ L22BackendProject/client/src/Hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
         setLoading(true);
         setError(null);
 
-        const response = await fetch('/api/user/signup', {
+        const response = await fetch('https://localhost:4000/api/user/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
@@ -32,5 +32,4 @@ export const useSignup = () => {
     }
 
     return { signup, error, loading }
-        
 };
